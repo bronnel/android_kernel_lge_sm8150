1844134e6ddb40c53c49fb62e6dac9beef008596
bronnel <austin.316scsa@yahoo.com>
2025-01-05 02:11:33 +0300

    Configuring the 4.x Kernel - USB

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Jan 5 02:05:34 +03 2025"
+#define UTS_VERSION "#5 SMP PREEMPT Sun Jan 5 02:10:30 +03 2025"
 #define LINUX_COMPILE_BY "matrix"
 #define LINUX_COMPILE_HOST "matrix-GS63VR-6RF"
 #define LINUX_COMPILER "Android (7612306, based on r428724) clang version 13.0.1 (https://android.googlesource.com/toolchain/llvm-project 6b0889cc0e9e868502342d9930a7717ff515e90b), GNU ld (binutils-2.27-bd24d23f) 2.27.0.20170315"
